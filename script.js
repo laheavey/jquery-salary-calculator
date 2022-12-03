ea57ddce77031676370f451415c67b7f55267740
Lauren <lauren.a.heavey@gmail.com>
2022-12-02 21:44:36 -0600

    Add function to re-color for costs over k.

--- script.js
+++ script.js
@@ -2,19 +2,21 @@ $(document).ready(readyNow);
 
 function readyNow () {
     $('#infoSubmissionButton').on('click', addInfoToArray);
+    // $('body').on('click', 'monthlyCosts', overBudget);
     renderTable ();
     // renderCosts();
 
 }
 
 let employeeInfoArray = [
-    {
-        firstName: 'Lauren',
-        lastName: 'Heavey',
-        idNumber: '1234',
-        jobTitle: 'professional',
-        annualSalary: 450000
-    }
+    // DUMMY DATA
+    // {
+    //     firstName: 'Lauren',
+    //     lastName: 'Heavey',
+    //     idNumber: '1234',
+    //     jobTitle: 'professional',
+    //     annualSalary: 450000
+    // }
 ];
 
 function addInfoToArray () {
@@ -79,7 +81,14 @@ function renderCosts () {
     $('#displayedCost').empty();
     calculateCosts();
     // Number(monthlyCosts);
-    $('#displayedCost').append(`${monthlyCosts}`);
+    $('#displayedCost').append(`$${monthlyCosts}`);
+    overBudget();
+}
+
+function overBudget () {    
+    if (monthlyCosts > 20000) {
+        $('#displayedCost').css({ "background-color": "red"});
+    }
 }
 
 // The application should have an input form that collects:
@@ -98,13 +107,11 @@ function renderCosts () {
 // For Base mode, it does **not** need to remove that Employee's salary from the 
 // reported total.
 
-    // 1. Create an input form that collects the above parameters w/ a submit button.
+    // 1. ✅ Create an input form that collects the above parameters w/ a submit button.
     //      a. ✅ Submit button grabs form info & stores it (in an array? or object?)
     //      b. ✅ Info added to table on the DOM
-    //      c. Submitted info used to calculate monthly costs
-    //      d. Monthly cost total appears on the DOM
-    //      e. Input fields are cleared
+    //      c. ✅ Input fields are cleared
     // 2. Calculate monthly costs using 1.a.
-    //      a. Add these to the DOM
+    //      a. ✅ Add these to the DOM
     //      b. If monthly cost total exceeds $20k, field background becomes red
     // 3. Create a delete button that removes employee from DOM
